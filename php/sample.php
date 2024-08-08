fcca38e41a8f1ed9480eea9bc75c38755390aae2
Ian Hickey <ianhickey@Ians-MacBook-Pro.local>
2024-08-08 12:56:48 -0700

    saving updates to build

---
+++ php/sample.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<a href="php/sample.php" download>Safe PHP Download</a>
+
+<div>
+<noscript>
+<?php
+	$name = "User!"
+	if (!empty($name)) {
+		echo "Welcome, $name";
+	}
+?>
+</noscript>
+</div>
+</body>
+</html>
